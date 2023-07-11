b21628183ad25c94b0040021c8ad1262b4c76db1
Dora Lee <dsa28s@naver.com>
2023-07-12 03:53:10 +0900

    wip: passkey implements

--- HyphenAuthenticate/Sources/HyphenAuthenticate.swift
+++ HyphenAuthenticate/Sources/HyphenAuthenticate.swift
@@ -1,8 +1,9 @@
+import AuthenticationServices
 import FirebaseAuth
 import Foundation
 @_spi(HyphenInternal) import HyphenCore
 
-public final class HyphenAuthenticate: NSObject {
+public final class HyphenAuthenticate: NSObject, ASAuthorizationControllerDelegate {
     public static let shared: HyphenAuthenticate = .init()
 
     override private init() {}
@@ -19,7 +20,60 @@ public final class HyphenAuthenticate: NSObject {
                 }
 
                 HyphenLogger.shared.logger.info("Firebase authenticate successfully. User -> \(result!.user.displayName ?? "")(\(result!.user.email ?? ""))")
+
+                let user = result!.user
+                user.getIDToken { token, _ in
+                    if let idToken = token {
+                        HyphenLogger.shared.logger.info("FIDToken -> \(idToken)")
+
+                        let challenge = "TEST".data(using: .utf8)!
+                        let userID = result!.user.uid.data(using: .utf8)!
+
+                        if #available(iOS 15.0, *) {
+                            let platformProvider = ASAuthorizationPlatformPublicKeyCredentialProvider(relyingPartyIdentifier: "api.dev.hyphen.at")
+                            // let platformKeyRequest = platformProvider.createCredentialRegistrationRequest(challenge: challenge, name: "\(result!.user.displayName ?? "")(\(result!.user.email ?? ""))", userID: userID)
+                            let assertionRequest = platformProvider.createCredentialAssertionRequest(challenge: challenge)
+                            let authController = ASAuthorizationController(authorizationRequests: [assertionRequest])
+                            authController.delegate = self
+                            authController.presentationContextProvider = self
+                            authController.performRequests()
+                        } else {
+                            // TODO: LEGACY
+                        }
+                    }
+                }
             }
         }
+    }
+
+    public func authorizationController(controller _: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if #available(iOS 15.0, *) {
+            if let credential = authorization.credential as? ASAuthorizationPlatformPublicKeyCredentialRegistration {
+                let attestationObject = credential.rawAttestationObject!
+                let clientDataJSON = credential.rawClientDataJSON
+                let credentialID = credential.credentialID
+
+            } else if let credential = authorization.credential as? ASAuthorizationPlatformPublicKeyCredentialAssertion {
+                HyphenLogger.shared.logger.info("\(String(data: credential.rawClientDataJSON.base64EncodedData(), encoding: .utf8))")
+                HyphenLogger.shared.logger.info("\(String(data: credential.rawAuthenticatorData.base64EncodedData(), encoding: .utf8))")
+                HyphenLogger.shared.logger.info("\(String(data: credential.signature.base64EncodedData(), encoding: .utf8))")
+            } else {
+                // Handle other authentication cases, such as Sign in with Apple.
+            }
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+
+    public func authorizationController(controller _: ASAuthorizationController, didCompleteWithError error: Error) {
+        HyphenLogger.shared.logger.error("\(error)")
+    }
+}
+
+// MARK: - ASAuthorizationControllerPresentationContextProviding
+
+extension HyphenAuthenticate: ASAuthorizationControllerPresentationContextProviding {
+    public func presentationAnchor(for _: ASAuthorizationController) -> ASPresentationAnchor {
+        UIApplication.shared.hyphensdk_currentKeyWindow!
     }
 }
