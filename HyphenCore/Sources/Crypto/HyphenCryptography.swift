317d898ea62f013618b8da86c8e7af863517ddd8
Dora Lee <dsa28s@naver.com>
2023-07-16 18:33:31 +0900

    feat(core): add cryptography manager

---
+++ HyphenCore/Sources/Crypto/HyphenCryptography.swift
@@ -0,0 +1,158 @@
+import Foundation
+import Security
+
+@_spi(HyphenInternal)
+public class HyphenCryptography: NSObject {
+    override private init() {}
+
+    static let privTag = Bundle.main.bundleIdentifier!
+    static let SecureEnclaveAccess = SecAccessControlCreateWithFlags(
+        kCFAllocatorDefault,
+        kSecAttrAccessibleWhenUnlockedThisDeviceOnly,
+        [.biometryAny, .privateKeyUsage],
+        nil
+    )
+
+    static var EnclaveAttribute: [String: Any] = [
+        kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+        kSecAttrKeySizeInBits as String: 256,
+        kSecAttrTokenID as String: kSecAttrTokenIDSecureEnclave,
+        kSecPrivateKeyAttrs as String: [
+            kSecAttrIsPermanent as String: true,
+            kSecAttrApplicationTag as String: privTag,
+            kSecAttrAccessControl as String: SecureEnclaveAccess!,
+        ],
+    ]
+
+    // kSecAttrAccessControl as String:    access
+    static var NonEnclaveAttribute: [String: Any] = [
+        kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+        kSecAttrKeySizeInBits as String: 256,
+        kSecPrivateKeyAttrs as String: [
+            kSecAttrIsPermanent as String: true,
+            kSecAttrApplicationTag as String: privTag,
+        ],
+    ]
+
+    public class func getPubKey() -> SecKey {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+            kSecAttrApplicationTag as String: privTag,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecReturnRef as String: true,
+        ]
+
+        var result: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+
+        if status != errSecSuccess {
+            HyphenLogger.shared.logger.error("priv key get failed.. generate new key")
+            generateKey()
+            SecItemCopyMatching(query as CFDictionary, &result)
+        }
+        let privKey: SecKey = result as! SecKey
+        let pubKey = SecKeyCopyPublicKey(privKey)!
+        return pubKey
+    }
+
+    public class func getPrivKey() -> SecKey {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+            kSecAttrApplicationTag as String: privTag,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecReturnRef as String: true,
+        ]
+
+        var result: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+        if status != errSecSuccess {
+            HyphenLogger.shared.logger.error("priv key get failed.. generate new key")
+            SecItemDelete(query as CFDictionary)
+            generateKey()
+            let new = SecItemCopyMatching(query as CFDictionary, &result)
+            print(new)
+        }
+        let privKey: SecKey = result as! SecKey
+        return privKey
+    }
+
+    public class func signData(_ data: Data) -> Data? {
+        let algorithm = SecKeyAlgorithm.ecdsaSignatureMessageX962SHA256
+        return SecKeyCreateSignature(getPrivKey(), algorithm, data as CFData, nil) as Data?
+    }
+
+    public class func deleteKey() {
+        let Privquery: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrKeyType as String: kSecAttrKeyTypeECSECPrimeRandom,
+            kSecAttrApplicationTag as String: privTag,
+            kSecAttrKeyClass as String: kSecAttrKeyClassPrivate,
+            kSecReturnRef as String: true,
+        ]
+        let code = SecItemDelete(Privquery as CFDictionary)
+        if code == errSecSuccess {
+            HyphenLogger.shared.logger.info("Key Delete Complete!")
+        } else {
+            HyphenLogger.shared.logger.error("Delete Failed!!")
+            print(code)
+        }
+    }
+
+    public class func generateKeyNoEnclave() {
+        var error: Unmanaged<CFError>?
+        // if device has not secure enclave, create normal keypair and save normal keychain
+        _ = SecKeyCreateRandomKey(NonEnclaveAttribute as CFDictionary, &error)
+    }
+
+    public class func generateKey() {
+        var error: Unmanaged<CFError>?
+        var privKey = SecKeyCreateRandomKey(EnclaveAttribute as CFDictionary, &error) // create Secure Enclave keypair
+        if privKey == nil { // if device has not secure enclave
+            HyphenLogger.shared.logger.critical("Secure Enclave Not Supported.")
+            privKey = SecKeyCreateRandomKey(NonEnclaveAttribute as CFDictionary, &error)
+        }
+    }
+//
+//    public class func encrypt(input: String) -> String? {
+//        let pubKey: SecKey = getPubKey()
+//        print(pubKey)
+//        var error: Unmanaged<CFError>?
+//
+//        let plain: CFData = input.data(using: .utf8)! as CFData
+//        let encData = SecKeyCreateEncryptedData(pubKey, SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM, plain, &error)
+//        var tdata: Data
+//        if encData == nil {
+//            print("encrypt error!!!")
+//            return nil
+//        } else {
+//            tdata = encData! as Data
+//        }
+//
+//        let b64result = tdata.base64EncodedString()
+//        return b64result
+//    }
+//
+//    public class func decrypt(input: String) -> String? {
+//        let privKey: SecKey = getPrivKey()
+//        print(privKey)
+//        guard let encData = Data(base64Encoded: input) else {
+//            print("decrypt error!!!")
+//            return nil
+//        }
+//        var error: Unmanaged<CFError>?
+//        let decData = SecKeyCreateDecryptedData(privKey, SecKeyAlgorithm.eciesEncryptionStandardX963SHA256AESGCM, encData as CFData, &error)
+//        var tdata: Data
+//
+//        if decData == nil {
+//            print("decrypt error!!!")
+//            return nil
+//        } else {
+//            tdata = decData! as Data
+//        }
+//
+//        let decResult = String(data: tdata, encoding: .utf8)!
+//        return decResult
+//    }
+}
