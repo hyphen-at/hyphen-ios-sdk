8765df8a5d436b1834d13c5f61b4128718236b9e
Dora Lee <dsa28s@naver.com>
2023-07-23 05:06:52 +0900

    feat(ui): add Notification Click Handler when app not running

--- HyphenUI/Sources/HyphenUI.swift
+++ HyphenUI/Sources/HyphenUI.swift
@@ -4,13 +4,25 @@ import UserNotifications
 @_spi(HyphenInternal) import HyphenCore
 
 public extension Hyphen {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+    func userNotificationCenter(_: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
         let userInfo = response.notification.request.content.userInfo
-        
+
+        await handleNotification(userInfo: userInfo)
+    }
+
+    func handleNotificationWhenAppNotRunning(userInfo: [AnyHashable: Any]) async {
+        await handleNotification(userInfo: userInfo)
+    }
+
+    private func handleNotification(userInfo: [AnyHashable: Any]) async {
         print(userInfo)
-        
+
         try? await Task.sleep(nanoseconds: 1_000_000_000)
-        
-        await UIApplication.shared.hyphensdk_currentKeyWindow?.rootViewController?.present(Hyphen2FAViewController(), animated: true)
+
+        let vc = await Hyphen2FAViewController()
+        await MainActor.run {
+            vc.isModalInPresentation = true
+        }
+        await UIApplication.shared.hyphensdk_currentKeyWindow?.rootViewController?.present(vc, animated: true)
     }
 }
