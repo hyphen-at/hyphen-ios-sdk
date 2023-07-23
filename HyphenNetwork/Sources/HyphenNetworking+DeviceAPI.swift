9ff15f5060ea6b4dace07cf062247bb9311148e1
Dora Lee <dsa28s@naver.com>
2023-07-23 17:08:00 +0900

    feat(ui): handling 2fa reject logic

--- HyphenNetwork/Sources/HyphenNetworking+DeviceAPI.swift
+++ HyphenNetwork/Sources/HyphenNetworking+DeviceAPI.swift
@@ -6,18 +6,23 @@ import Moya
 // MARK: - Hyphen Device API
 
 public extension HyphenNetworking {
+    
+    @discardableResult
     func editDevice(publicKey: String, payload: HyphenRequestEditDevice) async throws -> HyphenResponseEmpty {
         return try await deviceProvider.async.request(.editDevice(publicKey: publicKey, payload: payload))
     }
 
+    @discardableResult
     func retry2FA(id: String, payload: HyphenRequestRetry2FA) async throws -> HyphenResponseEmpty {
         return try await deviceProvider.async.request(.retry2FA(id: id, payload: payload))
     }
 
+    @discardableResult
     func deny2FA(id: String) async throws -> HyphenResponseEmpty {
         return try await deviceProvider.async.request(.deny2FA(id: id))
     }
 
+    @discardableResult
     func approve2FA(id: String, payload: HyphenRequest2FAApprove) async throws -> HyphenResponseEmpty {
         return try await deviceProvider.async.request(.approve2FA(id: id, payload: payload))
     }
--- HyphenUI/Sources/2FA/Hyphen2FAState.swift
+++ HyphenUI/Sources/2FA/Hyphen2FAState.swift
@@ -1,17 +1,38 @@
-import HyphenCore
+@_spi(HyphenInternal) import HyphenCore
 import SwiftUI
+import HyphenNetwork
 
 class Hyphen2FAState: ObservableObject {
     @Published var twoFactorAuth: Hyphen2FAStatus? = nil
 
-    @Published private var remainingTimeSeconds = 3 * 60
+    @Published var remainingTimeSeconds = 3 * 60
+    
+    @Published var isProcessing = false
 
     var remainingTimeText: String {
-        " (\(remainingTimeSeconds / 60):\(String(format: "%02d", remainingTimeSeconds % 60)))"
+        if remainingTimeSeconds == 0 {
+            return ""
+        }
+        
+        return " (\(remainingTimeSeconds / 60):\(String(format: "%02d", remainingTimeSeconds % 60)))"
     }
 
     init() {
         startCountdownTimer()
+    }
+    
+    func reject2FA() {
+        guard let twoFactorAuth = self.twoFactorAuth else {
+            HyphenLogger.shared.logger.critical("HyphenUI SDK internal error. UI state 'twoFactorAuth' is nil.")
+            return
+        }
+        
+        isProcessing = true
+        
+        Task {
+            try await HyphenNetworking.shared.deny2FA(id: twoFactorAuth.id)
+            await UIApplication.shared.hyphensdk_currentKeyWindowPresentedController?.dismiss(animated: true)
+        }
     }
 
     private func startCountdownTimer() {
